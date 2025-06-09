bf2a2336c118b741c61a3eddab6944f296a6a569
Дарья Окулова <dashaokulova170499@gmail.com>
2025-06-09 22:13:17 +0300

    Modify feed_data.ipynb and constants.py

--- config/constants.py
+++ config/constants.py
@@ -5,6 +5,5 @@ PROJECT_ROOT = Path(__file__).parent.parent
 DATA_DIR = PROJECT_ROOT / "data"
 PROCESSED_DIR = DATA_DIR / "processed"
 
-# Автоматическое создание папок при импорте
 DATA_DIR.mkdir(exist_ok=True)
 PROCESSED_DIR.mkdir(exist_ok=True)
